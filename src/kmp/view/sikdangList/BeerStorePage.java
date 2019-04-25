79b5655516fd012d524895f0faac20f20351dde8
YJ <YJ@yg>
2019-04-25 15:09:16 +0900

    맥주창고 + MapPage

--- src/kmp/view/sikdangList/BeerStorePage.java
+++ src/kmp/view/sikdangList/BeerStorePage.java
@@ -1,20 +1,27 @@
 package kmp.view.sikdangList;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 import kmp.view.ChangePanel;
 import kmp.view.MainFrame;
 import kmp.view.MainPage;
+import kmp.view.MapPage;
 import kmp.view.MemberInfoPage;
 import kmp.view.SikdangPage;
 
@@ -31,7 +38,7 @@ public class BeerStorePage extends JPanel{
 		Color backgcolor = new Color(234,255,236);
 		
 
-		//占쎄占쎌占쎈막 占쎌沃紐占 占쎈막占쎈
+		//�뜝�럡혰�뜝�럩혡혵�뜝�럥留� �뜝�럩혬亦꺜섓쭗혙�뜝 �뜝�럥留됧뜝�럥혯
 		Image homeImg = new ImageIcon("images/home.png").getImage().getScaledInstance(240, 50, 0);
 		Image sikImg = new ImageIcon("images/mRestaurant.png").getImage().getScaledInstance(100, 100, 0);
 		Image favoriteImg = new ImageIcon("images/mFavorites.png").getImage().getScaledInstance(100, 100, 0);
@@ -43,7 +50,7 @@ public class BeerStorePage extends JPanel{
 		
 		
 
-		//�긽�떒 �뙣�꼸
+		//占쎄맒占쎈뼊 占쎈솭占쎄섯
 		JPanel panel1 = new JPanel();
 		panel1.setSize(400, 150);
 		panel1.setLocation(0, 0);
@@ -60,7 +67,7 @@ public class BeerStorePage extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				ChangePanel.ChangePanel(mf,beerStorePage, new MainPage(mf));
+				ChangePanel.changePanel(mf,beerStorePage, new MainPage(mf));
 			}
 			
 		});
@@ -72,7 +79,7 @@ public class BeerStorePage extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				ChangePanel.ChangePanel(mf, beerStorePage, new MemberInfoPage(mf));
+				ChangePanel.changePanel(mf, beerStorePage, new MemberInfoPage(mf));
 			}
 			
 			
@@ -93,7 +100,7 @@ public class BeerStorePage extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				ChangePanel.ChangePanel(mf,beerStorePage, new SikdangPage(mf));
+				ChangePanel.changePanel(mf,beerStorePage, new SikdangPage(mf));
 			}
 			
 		});
@@ -116,25 +123,182 @@ public class BeerStorePage extends JPanel{
 		mf.add(this);
 		
 		JPanel panel2 = new JPanel();
-		panel2.setSize(350, 400);
-		panel2.setLocation(20, 200);
-		panel2.setBackground(color);
+		panel2.setSize(400, 550);
+		panel2.setLocation(0, 150);
+		panel2.setBackground(backgcolor);
 		
-
-		Image beername = new ImageIcon("images/beername.png").getImage().getScaledInstance(100, 20, 0);
-		JLabel beerLabel = new JLabel("쇰꺼");
-		beerLabel.setSize(350, 150);
-		beerLabel.setLocation(0, 0);
-		beerLabel.setBackground(Color.YELLOW);
-		JTextField beertext = new JTextField();
-		beertext.setText("留μ＜李쎄�");
-		beertext.setSize(50,50);
-		beertext.setLocation(10,10);
-		beerLabel.add(beertext);
 		
-		panel2.add(beerLabel);
+		//----------------------
+		Image beername = new ImageIcon("images/beername.png").getImage().getScaledInstance(100, 25, 0);
+		JPanel panel3 = new JPanel();
+		panel3.setSize(350, 65);
+		panel3.setLocation(20, 20);
+		panel3.setBackground(color);
+		JLabel beerLabel = new JLabel(new ImageIcon(beername));
+		beerLabel.setSize(100, 20);
+		beerLabel.setLocation(120, 0);
+		beerLabel.setBackground(color);
+		JTextArea num = new JTextArea("000-000-0000");
+		num.setSize(100, 20);
+		num.setLocation(15, 35);
+		JTextArea address = new JTextArea("강남구 역삼동");
+		address.setSize(120, 20);
+		address.setLocation(215, 35);
+		
+		panel3.add(beerLabel);
+		panel3.add(num);
+		panel3.add(address);
+		
+		//--------------------------------------------------------------
+		
+		String [] count = {"1","2","3","4","5"};
+		
+		JPanel panel4 = new JPanel();
+		panel4.setSize(350, 270);
+		panel4.setLocation(20, 85);
+		panel4.setBackground(Color.WHITE);
+		
+		JTextField menu1 = new JTextField("메뉴1");
+		menu1.setSize(100, 20);
+		menu1.setLocation(25, 25);
+		menu1.setEditable(false);
+		
+		JTextField menu1p = new JTextField("가격");
+		menu1p.setSize(70, 20);
+		menu1p.setLocation(160, 25);
+		menu1p.setEditable(false);
+		
+		List listmenu1 = new List(3, false);
+		listmenu1.add("1");
+		listmenu1.add("2");
+		listmenu1.add("3");
+		listmenu1.setLocation(270,25);
+		listmenu1.setSize(30,18);
+			listmenu1.setVisible(true);
+		
+		
+	
+		JTextField menu2 = new JTextField("메뉴2");
+		menu2.setSize(100, 20);
+		menu2.setLocation(25, 70);
+		menu2.setEditable(false);
+		
+		JTextField menu2p = new JTextField("가격");
+		menu2p.setSize(70, 20);
+		menu2p.setLocation(160, 70);
+		menu2p.setEditable(false);
+		
+		List listmenu2 = new List(3, false);
+		listmenu2.add("1");
+		listmenu2.add("2");
+		listmenu2.add("3");
+		listmenu2.setLocation(270,70);
+		listmenu2.setSize(30,18);
+			listmenu1.setVisible(true);
+		
+		
+		
+		JTextField menu3 = new JTextField("메뉴3");
+		menu3.setSize(100, 20);
+		menu3.setLocation(25, 110);
+		menu3.setEditable(false);
+		
+		JTextField menu3p = new JTextField("가격");
+		menu3p.setSize(70, 20);
+		menu3p.setLocation(160, 110);
+		menu3p.setEditable(false);
+		
+		List listmenu3 = new List(3, false);
+		listmenu3.add("1");
+		listmenu3.add("2");
+		listmenu3.add("3");
+		listmenu3.setLocation(270,110);
+		listmenu3.setSize(30,18);
+			listmenu1.setVisible(true);
+			
+		JTextField menu4 = new JTextField("메뉴4");
+		menu4.setSize(100, 20);
+		menu4.setLocation(25, 150);
+		menu4.setEditable(false);
+		
+		JTextField menu4p = new JTextField("가격");
+		menu4p.setSize(70, 20);
+		menu4p.setLocation(160, 150);
+		menu4p.setEditable(false);
+		
+		List listmenu4 = new List(3, false);
+		listmenu4.add("1");
+		listmenu4.add("2");
+		listmenu4.add("3");
+		listmenu4.setLocation(270,150);
+		listmenu4.setSize(30,18);
+			listmenu1.setVisible(true);
+			
+		JTextField total = new JTextField("합계");
+		total.setSize(100, 20);
+		total.setLocation(110,210);
+		
+		
+		
+		panel4.add(menu1);
+		panel4.add(menu1p);
+		panel4.add(listmenu1);
+		
+		panel4.add(menu2);
+		panel4.add(menu2p);
+		panel4.add(listmenu2);
+		
+		panel4.add(menu3);
+		panel4.add(menu3p);
+		panel4.add(listmenu3);
+		
+		
+		panel4.add(menu4);
+		panel4.add(menu4p);
+		panel4.add(listmenu4);
+		
+		
+		panel4.add(total);		
+		
+		//---------------------------------------
+		
+		/*JTextArea ta = new JTextArea("000-000-0000");
+		ta.setSize(100, 50);
+		ta.setLocation(20, 20);
+		ta.setBackground(Color.pink);
+		ta.setEditable(false);*/
+		
+		JPanel panel5 = new JPanel();
+		panel5.setSize(350, 120);
+		panel5.setLocation(20, 335);
+		panel5.setBackground(Color.GRAY);
+		
+		JButton map = new JButton("위치보기");
+		map.setSize(100 ,30 );
+		map.setLocation(35,50);
+		panel5.add(map);
+		map.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePanel.changePanel(mf, beerStorePage, new MapPage(mf));
+				
+			}
+		});
+		
+		
+		
+		
+		
+		
+		panel2.add(panel3);
+		panel2.add(panel4);
+		panel2.add(panel5);
+		
+		
 		this.add(panel1);
 		this.add(panel2);
+		
 		mf.add(this);
 		
 	}
