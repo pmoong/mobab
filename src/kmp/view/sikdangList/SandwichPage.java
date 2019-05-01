4b25582f0eab95273232add768ccd0a660275770
user1 <user1@KH_C>
2019-05-01 13:28:02 +0900

    위치보기 연결, 전화버튼 클릭 시 팝업

--- src/kmp/view/sikdangList/SandwichPage.java
+++ src/kmp/view/sikdangList/SandwichPage.java
@@ -18,6 +18,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
+import kmp.controller.Controller;
 import kmp.view.ChangePanel;
 import kmp.view.ChargePage;
 import kmp.view.FavoritePage;
@@ -41,7 +42,7 @@ public class SandwichPage extends JPanel {
 		Color backgcolor = new Color(234,255,236);
 
 
-		//»ç¿ëÇÒ ÀÌ¹ÌÁö ¼±¾ð
+		//ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ì¹ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 		Image homeImg = new ImageIcon("images/home.png").getImage().getScaledInstance(240, 50, 0);
 		Image sikImg = new ImageIcon("images/mRestaurant.png").getImage().getScaledInstance(100, 100, 0);
 		Image favoriteImg = new ImageIcon("images/mFavorites.png").getImage().getScaledInstance(100, 100, 0);
@@ -53,7 +54,7 @@ public class SandwichPage extends JPanel {
 
 
 
-		//»ó´ÜÆÐ³Î
+		//ï¿½ï¿½ï¿½ï¿½Ð³ï¿½
 		JPanel panel1 = new JPanel();
 		panel1.setSize(400, 150);
 		panel1.setLocation(0, 0);
@@ -181,7 +182,7 @@ public class SandwichPage extends JPanel {
 		JTextArea num = new JTextArea("000-000-0000");
 		num.setSize(100, 20);
 		num.setLocation(15, 35);
-		JTextArea address = new JTextArea("°­³²±¸ ¿ª»ïµ¿");
+		JTextArea address = new JTextArea("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ïµ¿");
 		address.setSize(120, 20);
 		address.setLocation(215, 35);
 
@@ -198,12 +199,12 @@ public class SandwichPage extends JPanel {
 		panel4.setLocation(20, 85);
 		panel4.setBackground(Color.WHITE);
 
-		JTextField menu1 = new JTextField("¸Þ´º1");
+		JTextField menu1 = new JTextField("ï¿½Þ´ï¿½1");
 		menu1.setSize(100, 20);
 		menu1.setLocation(25, 25);
 		menu1.setEditable(false);
 
-		JTextField menu1p = new JTextField("°¡°Ý");
+		JTextField menu1p = new JTextField("ï¿½ï¿½ï¿½ï¿½");
 		menu1p.setSize(70, 20);
 		menu1p.setLocation(160, 25);
 		menu1p.setEditable(false);
@@ -218,12 +219,12 @@ public class SandwichPage extends JPanel {
 
 
 
-		JTextField menu2 = new JTextField("¸Þ´º2");
+		JTextField menu2 = new JTextField("ï¿½Þ´ï¿½2");
 		menu2.setSize(100, 20);
 		menu2.setLocation(25, 70);
 		menu2.setEditable(false);
 
-		JTextField menu2p = new JTextField("°¡°Ý");
+		JTextField menu2p = new JTextField("ï¿½ï¿½ï¿½ï¿½");
 		menu2p.setSize(70, 20);
 		menu2p.setLocation(160, 70);
 		menu2p.setEditable(false);
@@ -238,12 +239,12 @@ public class SandwichPage extends JPanel {
 
 
 
-		JTextField menu3 = new JTextField("¸Þ´º3");
+		JTextField menu3 = new JTextField("ï¿½Þ´ï¿½3");
 		menu3.setSize(100, 20);
 		menu3.setLocation(25, 110);
 		menu3.setEditable(false);
 
-		JTextField menu3p = new JTextField("°¡°Ý");
+		JTextField menu3p = new JTextField("ï¿½ï¿½ï¿½ï¿½");
 		menu3p.setSize(70, 20);
 		menu3p.setLocation(160, 110);
 		menu3p.setEditable(false);
@@ -256,12 +257,12 @@ public class SandwichPage extends JPanel {
 		listmenu3.setSize(30,18);
 		listmenu1.setVisible(true);
 
-		JTextField menu4 = new JTextField("¸Þ´º4");
+		JTextField menu4 = new JTextField("ï¿½Þ´ï¿½4");
 		menu4.setSize(100, 20);
 		menu4.setLocation(25, 150);
 		menu4.setEditable(false);
 
-		JTextField menu4p = new JTextField("°¡°Ý");
+		JTextField menu4p = new JTextField("ï¿½ï¿½ï¿½ï¿½");
 		menu4p.setSize(70, 20);
 		menu4p.setLocation(160, 150);
 		menu4p.setEditable(false);
@@ -274,7 +275,7 @@ public class SandwichPage extends JPanel {
 		listmenu4.setSize(30,18);
 		listmenu1.setVisible(true);
 
-		JTextField total = new JTextField("ÇÕ°è");
+		JTextField total = new JTextField("ï¿½Õ°ï¿½");
 		total.setSize(100, 20);
 		total.setLocation(110,210);
 
@@ -313,7 +314,7 @@ public class SandwichPage extends JPanel {
 		panel5.setLocation(20, 335);
 		panel5.setBackground(Color.GRAY);
 
-		JButton map = new JButton("À§Ä¡º¸±â");
+		JButton map = new JButton("ï¿½ï¿½Ä¡ï¿½ï¿½ï¿½ï¿½");
 		map.setSize(100 ,30 );
 		map.setLocation(35,50);
 		panel5.add(map);
@@ -321,8 +322,9 @@ public class SandwichPage extends JPanel {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				ChangePanel.changePanel(mf, sandwichPage, new MapPage(mf));
-
+				ChangePanel.changePanel(mf, sandwichPage, new MapPage(mf, 4));
+	               Controller ctr = new Controller();
+	               ctr.map(4);
 			}
 		});
 
